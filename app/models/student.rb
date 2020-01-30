68f17b5b167a8dd807b33a320de4f01b977874b5
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-01-30 14:22:31 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ActiveRecord::Base
+  self.table_name = "students"
+  # attr_accessor :active
+  attribute :active, :boolean, default: false
+  
   def to_s
     self.first_name + " " + self.last_name
   end
--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,9 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :first_name
+      t.string :first_na me
       t.string :last_name
-
+      t.boolean :active, default: false, null: false
       t.timestamps null: false
     end
   end
