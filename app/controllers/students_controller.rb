625ac435539f7535c80b2fa2f21b1cdd1151af6a
Sarah Condon <s.j.condon01@gmail.com>
2020-02-10 15:52:30 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,16 +8,13 @@ class StudentsController < ApplicationController
   def show
   end
 
-  def activate
-    @student = Student.find(params[:id])
-    if @student.active 
-      @student.active = false
-    else
-      @student.active = true
-    end
+  def activate 
+    @student = Student.find_by(id: params[:id])
+    @student.active = !@student.active
     @student.save
     redirect_to student_path(@student)
   end
+
 
   private
 
